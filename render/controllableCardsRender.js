54ed17c54635f52c7a1b781f6e66987089752333
Petr Heinz <petr@betterstack.com>
2023-03-27 00:56:32 +0200

    Extract methods for drag&drop

--- render/controllableCardsRender.js
+++ render/controllableCardsRender.js
@@ -8,6 +8,7 @@ const MOVEMENT_TOLERANCE = 5
 
 export default class ControllableCardsRender extends CardsRender {
     vars = {
+        root: document.body,
         draggedElement: null,
         draggedMovement: 0,
         isInInputState: false,
@@ -20,85 +21,98 @@ export default class ControllableCardsRender extends CardsRender {
 
     initialize(root) {
         super.initialize(root);
+        this.vars.root = root
 
         document.addEventListener("mousedown", (event) => {
             if (event.button !== 0) return
             if (!this.vars.isInInputState) return
-
-            const draggedElement = document.createElement("div")
-            const actionCardIndex = getChildIndex(this.actionCards, event.target)
-            if (actionCardIndex !== null) {
-                draggedElement.append(this.actionCards.children[actionCardIndex].cloneNode(true))
-                draggedElement.dataset.selected = ROBOT_CARDS_ACTION
-                draggedElement.dataset.selectedIndex = actionCardIndex
-                draggedElement.querySelector(".hand-toggle")?.remove()
-            }
-            const handCardIndex = getChildIndex(this.handCards, event.target)
-            if (handCardIndex !== null) {
-                draggedElement.append(this.handCards.children[handCardIndex].cloneNode(true))
-                draggedElement.dataset.selected = ROBOT_CARDS_HAND
-                draggedElement.dataset.selectedIndex = handCardIndex
-            }
-            if (draggedElement.children.length === 0) return
-
-            if (this.vars.draggedElement !== null) {
-                this.vars.draggedElement.remove()
-            }
-            this.vars.draggedElement = draggedElement
-            this.vars.draggedMovement = 0
-
-            draggedElement.style.position = "fixed"
-            draggedElement.style.pointerEvents = "none"
-            draggedElement.style.zIndex = "100"
-
-            draggedElement.style.transform = `translate(-50%, -50%)`
-            draggedElement.style.left = `${event.clientX}px`
-            draggedElement.style.top = `${event.clientY}px`
+            this._dragStart(event.target, event.clientX, event.clientY);
         })
         document.addEventListener("mousemove", (event) => {
-            const draggedElement = this.vars.draggedElement
-            if (draggedElement === null) return
-
-            this.vars.draggedMovement += Math.abs(event.movementX) + Math.abs(event.movementY)
-
-            draggedElement.style.left = `${event.clientX}px`
-            draggedElement.style.top = `${event.clientY}px`
-
-            if (this.vars.draggedMovement > MOVEMENT_TOLERANCE && draggedElement.parentElement === null) {
-                root.append(draggedElement)
-            }
+            this._dragMove(event.clientX, event.clientY);
         })
         document.addEventListener("mouseup", (event) => {
-            const draggedElement = this.vars.draggedElement
-            if (draggedElement === null) return
-
-            draggedElement.remove()
-            this.vars.draggedElement = null
-
-            const actionCardIndex = getChildIndex(this.actionCards, event.target)
-            if (draggedElement.dataset.selected === ROBOT_CARDS_HAND) {
-                if (actionCardIndex !== null) {
-                    this.robotControl.chooseAction(draggedElement.dataset.selectedIndex, actionCardIndex)
-                    return
-                }
-            }
-            if (draggedElement.dataset.selected === ROBOT_CARDS_ACTION) {
-                if (actionCardIndex === null) {
-                    this.robotControl.discardAction(draggedElement.dataset.selectedIndex)
-                    return
-                }
-                if (`${actionCardIndex}` === draggedElement.dataset.selectedIndex) {
-                    this.robotControl.toggleActionHand(actionCardIndex)
-                    return
-                }
-                this.robotControl.swapActions(draggedElement.dataset.selectedIndex, actionCardIndex)
-            }
+            this._dragStop(event.target)
         })
 
         this.readyButton.addEventListener("click", () => {
             if (!this.vars.isInInputState) return
             this.robotControl.commit()
         })
+    }
+
+    _dragStart(target, x, y) {
+        const draggedElement = document.createElement("div")
+        const actionCardIndex = getChildIndex(this.actionCards, target)
+        if (actionCardIndex !== null) {
+            draggedElement.append(this.actionCards.children[actionCardIndex].cloneNode(true))
+            draggedElement.dataset.selected = ROBOT_CARDS_ACTION
+            draggedElement.dataset.selectedIndex = actionCardIndex
+            draggedElement.querySelector(".hand-toggle")?.remove()
+        }
+        const handCardIndex = getChildIndex(this.handCards, target)
+        if (handCardIndex !== null) {
+            draggedElement.append(this.handCards.children[handCardIndex].cloneNode(true))
+            draggedElement.dataset.selected = ROBOT_CARDS_HAND
+            draggedElement.dataset.selectedIndex = handCardIndex
+        }
+        if (draggedElement.children.length === 0) return
+
+        if (this.vars.draggedElement !== null) {
+            this.vars.draggedElement.remove()
+        }
+        this.vars.draggedElement = draggedElement
+
+        this.vars.draggedMovement = 0
+        draggedElement.style.position = "fixed"
+        draggedElement.style.pointerEvents = "none"
+
+        draggedElement.style.zIndex = "100"
+        draggedElement.style.transform = `translate(-50%, -50%)`
+        draggedElement.style.left = `${x}px`
+        draggedElement.style.top = `${y}px`
+    }
+
+    _dragMove(x, y) {
+        const draggedElement = this.vars.draggedElement
+        if (draggedElement === null) return
+
+        this.vars.draggedMovement += Math.abs(x - parseInt(draggedElement.style.left))
+        this.vars.draggedMovement += Math.abs(y - parseInt(draggedElement.style.top))
+
+        draggedElement.style.left = `${x}px`
+        draggedElement.style.top = `${y}px`
+
+        if (this.vars.draggedMovement > MOVEMENT_TOLERANCE && draggedElement.parentElement === null) {
+            root.append(draggedElement)
+        }
+    }
+
+    _dragStop(target) {
+        const draggedElement = this.vars.draggedElement
+        if (draggedElement === null) return
+
+        draggedElement.remove()
+        this.vars.draggedElement = null
+
+        const actionCardIndex = getChildIndex(this.actionCards, target)
+        if (draggedElement.dataset.selected === ROBOT_CARDS_HAND) {
+            if (actionCardIndex !== null) {
+                this.robotControl.chooseAction(draggedElement.dataset.selectedIndex, actionCardIndex)
+                return
+            }
+        }
+        if (draggedElement.dataset.selected === ROBOT_CARDS_ACTION) {
+            if (actionCardIndex === null) {
+                this.robotControl.discardAction(draggedElement.dataset.selectedIndex)
+                return
+            }
+            if (`${actionCardIndex}` === draggedElement.dataset.selectedIndex) {
+                this.robotControl.toggleActionHand(actionCardIndex)
+                return
+            }
+            this.robotControl.swapActions(draggedElement.dataset.selectedIndex, actionCardIndex)
+        }
     }
 
     initAction(root, action) {
