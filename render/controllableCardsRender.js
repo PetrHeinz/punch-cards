4730cde2a696534a4490421a2a653a6261a2fcdd
Petr Heinz <petr@betterstack.com>
2023-03-27 01:21:02 +0200

    Support drag&drop via touch on mobile

--- render/controllableCardsRender.js
+++ render/controllableCardsRender.js
@@ -26,14 +26,31 @@ export default class ControllableCardsRender extends CardsRender {
         document.addEventListener("mousedown", (event) => {
             if (event.button !== 0) return
             if (!this.vars.isInInputState) return
-            this._dragStart(event.target, event.clientX, event.clientY);
-        })
+            this._dragStart(event.target, event.clientX, event.clientY)
+                && event.preventDefault()
+        }, {passive: false})
+        document.addEventListener("touchstart", (event) => {
+            if (!this.vars.isInInputState) return
+            this._dragStart(event.target, event.touches[0].clientX, event.touches[0].clientY)
+                && event.preventDefault()
+        }, {passive: false})
         document.addEventListener("mousemove", (event) => {
-            this._dragMove(event.clientX, event.clientY);
-        })
+            this._dragMove(event.clientX, event.clientY)
+                && event.preventDefault()
+        }, {passive: false})
+        document.addEventListener("touchmove", (event) => {
+            this._dragMove(event.changedTouches[0].clientX, event.changedTouches[0].clientY)
+                && event.preventDefault()
+        }, {passive: false})
         document.addEventListener("mouseup", (event) => {
             this._dragStop(event.target)
-        })
+                && event.preventDefault()
+        }, {passive: false})
+        document.addEventListener("touchend", (event) => {
+            const target = document.elementFromPoint(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+            this._dragStop(target)
+                && event.preventDefault()
+        }, {passive: false})
 
         this.readyButton.addEventListener("click", () => {
             if (!this.vars.isInInputState) return
@@ -43,6 +60,7 @@ export default class ControllableCardsRender extends CardsRender {
 
     _dragStart(target, x, y) {
         const draggedElement = document.createElement("div")
+        console.log("dragstart", draggedElement)
         const actionCardIndex = getChildIndex(this.actionCards, target)
         if (actionCardIndex !== null) {
             draggedElement.append(this.actionCards.children[actionCardIndex].cloneNode(true))
@@ -56,7 +74,7 @@ export default class ControllableCardsRender extends CardsRender {
             draggedElement.dataset.selected = ROBOT_CARDS_HAND
             draggedElement.dataset.selectedIndex = handCardIndex
         }
-        if (draggedElement.children.length === 0) return
+        if (draggedElement.children.length === 0) return false
 
         if (this.vars.draggedElement !== null) {
             this.vars.draggedElement.remove()
@@ -71,11 +89,13 @@ export default class ControllableCardsRender extends CardsRender {
         draggedElement.style.transform = `translate(-50%, -50%)`
         draggedElement.style.left = `${x}px`
         draggedElement.style.top = `${y}px`
+
+        return true
     }
 
     _dragMove(x, y) {
         const draggedElement = this.vars.draggedElement
-        if (draggedElement === null) return
+        if (draggedElement === null) return false
 
         this.vars.draggedMovement += Math.abs(x - parseInt(draggedElement.style.left))
         this.vars.draggedMovement += Math.abs(y - parseInt(draggedElement.style.top))
@@ -86,11 +106,13 @@ export default class ControllableCardsRender extends CardsRender {
         if (this.vars.draggedMovement > MOVEMENT_TOLERANCE && draggedElement.parentElement === null) {
             root.append(draggedElement)
         }
+
+        return true
     }
 
     _dragStop(target) {
         const draggedElement = this.vars.draggedElement
-        if (draggedElement === null) return
+        if (draggedElement === null) return false
 
         draggedElement.remove()
         this.vars.draggedElement = null
@@ -99,20 +121,23 @@ export default class ControllableCardsRender extends CardsRender {
         if (draggedElement.dataset.selected === ROBOT_CARDS_HAND) {
             if (actionCardIndex !== null) {
                 this.robotControl.chooseAction(draggedElement.dataset.selectedIndex, actionCardIndex)
-                return
+                return true
             }
         }
         if (draggedElement.dataset.selected === ROBOT_CARDS_ACTION) {
             if (actionCardIndex === null) {
                 this.robotControl.discardAction(draggedElement.dataset.selectedIndex)
-                return
+                return true
             }
             if (`${actionCardIndex}` === draggedElement.dataset.selectedIndex) {
                 this.robotControl.toggleActionHand(actionCardIndex)
-                return
+                return true
             }
             this.robotControl.swapActions(draggedElement.dataset.selectedIndex, actionCardIndex)
+            return true
         }
+
+        return false
     }
 
     initAction(root, action) {
